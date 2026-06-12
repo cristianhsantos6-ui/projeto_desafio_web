9c77abe11aa5162906891a96aec9c35da277779b
Tarde Cetafaju <Tarde.Cetafaju@labfies.org.br>
2026-06-12 16:02:22 -0300

    atualizar

--- js/script_idade.js
+++ js/script_idade.js
@@ -1,6 +1,24 @@
-//PEGANDO ELEMENYOS DO DOM
-   const formando = document.queryselector('informulario-pessoa')
-   const divResultado = document.querySelector('iv-dados>')
+///PEGANDO ELEMENTOS DO DOM
+const formDados = document.querySelector('#formulario-pessoa')
+const divResultado = document.querySelector('#div-dados')
 
-   //CAPTURANDO O EVENTO SUBMIT DO FORMULÁRIO
-   formDADOS.addEventlistener('submit', (evt)=>)
+//CAPTURANDO O EVENTO SUBMIT DO FORMULÁRIO
+formDados.addEventListener('submit', (evt)=>{
+    evt.preventDefault()
+
+    const objFormDados = new FormData(formDados)
+
+    let nome = objFormDados.get('nome')
+    let idade = objFormDados.get('idade')
+
+    let situacaoIdade = ''
+
+    if (idade >=18){
+        situacaoIdade = `${nome}, vc é maior de idade`
+    }else{
+        situacaoIdade = `${nome}, vc é menor de idade`
+    }
+
+    divResultado.innerHTML = situacaoIdade
+    
+    })
